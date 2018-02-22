3783aacffc75a5a3a1d950d7d50d9cf96a04cba5
Cherie Lum <charismatic.cherie@gmail.com>
2018-02-22 14:59:30 -0500

    initializing

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, AppRegistry, Image } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}}/>
+      
+    );
+    return (
+      <View style={styles.container}>
+       <Text>In The Park</Text>
+      </View>
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
